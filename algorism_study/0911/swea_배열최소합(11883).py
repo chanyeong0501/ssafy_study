a1e100ab0c6a6b9f771f72bc0f93e9856ceb12fc
yeomchanyeong <changg942@naver.com>
2026-09-11 17:15:00 +0900

    0911

---
+++ algorism_study/0911/swea_배열최소합(11883).py
@@ -0,0 +1,18 @@
+# 순열이라는게 결국 하나하나 줄세우기를 하는 것을 말함 그래서 우리가 알고 있는 배열 즉 arr이 있다고 하면 그걸
+# 임의의 배열인 [0]으로 만든 배열을 통해서
+
+import sys
+sys.stdin = open("swea_배열최소합(11883).txt")
+
+def perm(lev):
+    if lev == N:
+        print(path)
+    else:
+
+
+T = int(input())
+for tc in range(1, 1+T):
+    N = int(input())
+    arr = list(map(int, input().split()))
+    path =
+    perm(0)
